13d951f3689b7bcf044c55f6f92e614abc00c300
Dictator <aouvarov8@gmail.com>
2021-03-10 18:10:57 +0200

    Implement ability to spawn and move 2 types of pipes:green and red

--- Assets/Scripts/PipesSpawner.cs
+++ Assets/Scripts/PipesSpawner.cs
@@ -6,9 +6,11 @@ public class PipesSpawner : MonoBehaviour
 {
 
     [SerializeField] float timeToSpawn = 1f;
-    [SerializeField] GameObject pipe;
+    [SerializeField] GameObject pipe1;
+    [SerializeField] GameObject pipe2;
     [SerializeField] float height;
 
+    private int pipeNum = 0;
     private Coroutine spawnPipesCorontine;
 
     // Start is called before the first frame update
@@ -16,12 +18,25 @@ public class PipesSpawner : MonoBehaviour
     {
         while (true)
         {
-            GameObject newPipe = Instantiate(pipe);
-            Vector3 randFactor = new Vector3(0, Random.Range(-height, height), 0);
-            newPipe.transform.position = transform.position + randFactor;
-            Destroy(newPipe, 5f); // 5f - Время до уничтоженния объекта-трубы (5c)
+            if (pipeNum == 0)
+            {
+                CreateAndMove(pipe1);
+            }
+            else if (pipeNum == 1)
+            {
+                CreateAndMove(pipe2);
+            }
             yield return new WaitForSeconds(timeToSpawn);
+            pipeNum = 1 - pipeNum;
         }
+    }
+
+    private void CreateAndMove(GameObject pipe)
+    {
+        GameObject newPipe = Instantiate(pipe);
+        Vector3 randFactor = new Vector3(0, Random.Range(-height, height), 0);
+        newPipe.transform.position = transform.position + randFactor;
+        Destroy(newPipe, 3f); // 5f - Время до уничтоженния объекта-трубы (5c)
     }
 
     // Update is called once per frame
